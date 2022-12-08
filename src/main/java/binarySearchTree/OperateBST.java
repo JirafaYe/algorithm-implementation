ef909c84a95fe6cd5d1b0b5b1a0d119f26ee0659
JirafaYe <2582101458@qq.com>
2022-12-08 18:55:58 +0800

    更改目录结构

--- src/main/java/binarySearchTree/OperateBST.java
+++ src/main/java/binarySearchTree/OperateBST.java
@@ -1,4 +1,7 @@
+package binarySearchTree;
+
 import node.TreeNode;
+
 
 public class OperateBST {
     public TreeNode insertBST(TreeNode root,int val){
@@ -7,14 +10,30 @@ public class OperateBST {
 
         if(root.val>val)
             root.left=insertBST(root.left,val);
-        if(root.val<=val)
+        if(root.val<val)
             root.right=insertBST(root.right,val);
 
         return root;
     }
 
     public TreeNode deleteBST(TreeNode root,int val){
-        return null;
+        if (root == null) return null;
+
+        if (root.val > val) {
+            root.left = deleteBST(root.left,val);
+        } else if (root.val < val) {
+            root.right = deleteBST(root.right,val);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode tmp = root.right;
+            while (tmp.left != null) {
+                tmp = tmp.left;
+            }
+            root.val = tmp.val;
+            root.right = deleteBST(root.right,tmp.val);
+        }
+        return root;
     }
 
     private TreeNode searchBST(TreeNode root,int val){
---
+++ src/test/java/SwingTest.java
@@ -0,0 +1,28 @@
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+public class SwingTest {
+
+    @Test
+    public void testSwing(){
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(400,300);
+        jFrame.setLocation(400,300);
+        jFrame.setVisible(true);
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(400,300);
+        jFrame.setLocation(400,300);
+        jFrame.setTitle("hello");
+
+        jFrame.setVisible(true);
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
